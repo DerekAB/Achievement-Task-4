16f428e3cfb674d0aa4254be3b92a03124962b76
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-23 18:47:27 +0000

    finished temperature

--- Task 4.py
+++ Task 4.py
@@ -11,8 +11,31 @@
 #from Celcius to Fahrenheit or vice versa, or if they want to 
 #from KPH to MPH or vice versa.
 
-decision = input("Would you like to convert temperature or speed: \n Temperature [1] \n Speed [2] \n").strip()
+def temperature(unit, number):
+    global conversion 
+    if unit == 'c':
+        conversion = (number * 1.8) + 32
+        roundedConversion = round(conversion, 2)
+        print(str(roundedConversion) + ' F')
+        return True
+    
+    
+    if unit == 'f':
+        conversion = (number - 32) * 0.5556
+        roundedConversion = round(conversion, 2)
+        print(str(roundedConversion) + ' C')
+        return True
+        
+        
+
+decision = float(input("Would you like to convert temperature or speed: \n Temperature [1] \n Speed [2] \n"))
 
 if decision == 1:
-    print('You have selected Temperature.')
+    unit = input("What is the unit you are converting from? 'C' for Celcius or 'F' for Fahrenheit:  ").strip().lower()
+    number = int(input("What is the temperature?: "))
+    temperature(unit, number)
+    
+if decision == 2:
+    unit = input("What is the unit you are converting from? 'KPH' for Kilometers Per Hour to Miles Per Hour, or 'MPH' for Miles Per Hour to Kilometers Per Hour: ").strip().lower()
+    
     
